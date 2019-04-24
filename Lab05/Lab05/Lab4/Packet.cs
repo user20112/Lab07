ef1179e39e3403f630233c09192434e5827decf8
DragonRider31174 <47368879+DragonRider31174@users.noreply.github.com>
2019-04-24 11:58:21 -0400

    1st commit

---
+++ Lab05/Lab05/Lab4/Packet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Packet
+    {
+        byte _dest, _src, _size, CRC;
+        const byte _SOH = 1;
+
+        public void packet()
+        {
+
+
+
+        }
+
+        public byte[] getPacket()
+        {
+            byte 
+
+
+
+        }
+
+    }
+}
